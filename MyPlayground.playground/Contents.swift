187d655fd620d9353c0907b1d512c4729c14ca01
regoky <Kyle.Rego@techconsulting.tech>
2024-10-24 09:29:57 -0400

    First xcode playground

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,30 @@
+import UIKit
+
+var greeting:String = "Hello, playground"
+print(greeting)
+
+var a:String = "dhdjhdh"
+print(a)
+
+var b:Int = 10
+print(b)
+
+var c:[Int] = [1, 2, 3]
+print(c)
+
+var df = [1, 0]
+var fr = [2, 3]
+var isSame = df == fr
+print(isSame)
+
+var array:[Character] = ["a", "b", "c"]
+array = ["b"]
+array.append("d")
+print(array.count)
+
+var d:Bool = true
+print(d)
+
+var newString = "\(a) \(b) \(c) \(array)"
+
+print(newString)
